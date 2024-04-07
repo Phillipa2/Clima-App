3ba48b66869f51aff57007d55e9bd1b972533bf7
Phillipa <phillipaatieno7@gmail.com>
2024-04-08 01:38:56 +0300

    Updated the html file, injected an image in the index.js file and updated my style.css file

--- src/index.js
+++ src/index.js
@@ -6,14 +6,19 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = newDate(response.data.time * 1000);
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temp.humidity}%`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -45,4 +50,4 @@ function handleSearchSubmit(event) {
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Paris");
+searchCity("city");
